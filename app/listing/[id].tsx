ab0a5be28d302972af5f1d470f8c7a47493dc8f0
vinh <vinhntse2002@gmail.com>
2024-06-24 15:41:37 +0700

    vegy

---
+++ app/listing/[id].tsx
@@ -0,0 +1,416 @@
+import {
+  Dimensions,
+  Image,
+  Modal,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { ListingType } from "@/types/listingType";
+import listingData from "@/data/destinations.json";
+import {
+  Feather,
+  FontAwesome,
+  FontAwesome5,
+  Ionicons,
+} from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+import Animated, {
+  SlideInDown,
+  interpolate,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useScrollViewOffset,
+} from "react-native-reanimated";
+
+const { width } = Dimensions.get("window");
+const IMG_HEIGHT = 300;
+
+const slots = Array.from({ length: 20 }, (_, i) => `Slot ${i + 1}`); // Sample slots
+
+const ListingDetails = () => {
+  const { id } = useLocalSearchParams();
+  const listing: ListingType = (listingData as ListingType[]).find(
+    (item) => item.id === id
+  );
+
+  const router = useRouter();
+
+  const scrollRef = useAnimatedRef<Animated.ScrollView>();
+  const scrollOffset = useScrollViewOffset(scrollRef);
+  const imageAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: interpolate(
+            scrollOffset.value,
+            [-IMG_HEIGHT, 0, IMG_HEIGHT],
+            [-IMG_HEIGHT / 2, 0, IMG_HEIGHT * 0.75]
+          ),
+        },
+        {
+          scale: interpolate(
+            scrollOffset.value,
+            [-IMG_HEIGHT, 0, IMG_HEIGHT],
+            [2, 1, 1]
+          ),
+        },
+      ],
+    };
+  });
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedSlot, setSelectedSlot] = useState<string | null>(null);
+
+  const handleBookNow = () => {
+    if (selectedSlot) {
+      // Proceed with booking logic
+      setModalVisible(false);
+      console.log(`Booking slot: ${selectedSlot}`);
+    } else {
+      // Show a message to select a slot
+      console.log("Please select a slot");
+    }
+  };
+
+  const renderSlots = () => {
+    const rows = [];
+    for (let i = 0; i < slots.length; i += 4) {
+      rows.push(
+        <View key={i} style={styles.slotRow}>
+          {slots.slice(i, i + 4).map((slot) => (
+            <TouchableOpacity
+              key={slot}
+              style={[
+                styles.slotItem,
+                slot === selectedSlot && styles.selectedSlotItem,
+              ]}
+              onPress={() => setSelectedSlot(slot)}
+            >
+              <Text
+                style={{
+                  color: slot === selectedSlot ? Colors.white : Colors.black,
+                }}
+              >
+                <Image
+                  source={require("../../assets/images/farm/field-have.png")}
+                  style={{
+                    marginTop: 5,
+                    width: 30,
+                    height: 30,
+                  }}
+                />
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      );
+    }
+    return rows;
+  };
+
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          headerTransparent: true,
+          headerTitle: "",
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => router.back()}
+              style={{
+                backgroundColor: "rgba(255, 255, 255, 0.5)",
+                borderRadius: 10,
+                padding: 4,
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: Colors.white,
+                  padding: 6,
+                  borderRadius: 10,
+                }}
+              >
+                <Feather name="arrow-left" size={20} />
+              </View>
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                backgroundColor: "rgba(255, 255, 255, 0.5)",
+                borderRadius: 10,
+                padding: 4,
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: Colors.white,
+                  padding: 6,
+                  borderRadius: 10,
+                }}
+              >
+                <Ionicons name="bookmark-outline" size={20} />
+              </View>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <View style={styles.container}>
+        <Animated.ScrollView
+          ref={scrollRef}
+          contentContainerStyle={{ paddingBottom: 150 }}
+        >
+          <Animated.Image
+            source={{ uri: listing.image }}
+            style={[styles.image, imageAnimatedStyle]}
+          />
+          <View style={styles.contentWrapper}>
+            <Text style={styles.listingName}>{listing.name}</Text>
+            <View style={styles.listingLocationWrapper}>
+              <FontAwesome5
+                name="map-marker-alt"
+                size={18}
+                color={Colors.primaryColor}
+              />
+              <Text style={styles.listingLocationTxt}>{listing.location}</Text>
+            </View>
+
+            <View style={styles.highlightWrapper}>
+              <View style={{ flexDirection: "row" }}>
+                <View style={styles.highlightIcon}>
+                  <Ionicons name="time" size={18} color={Colors.primaryColor} />
+                </View>
+                <View>
+                  <Text style={styles.highlightTxt}>Thời gian đăng</Text>
+                  <Text style={styles.highlightTxtVal}>
+                    {listing.duration} Days
+                  </Text>
+                </View>
+              </View>
+              <View style={{ flexDirection: "row" }}>
+                <View style={styles.highlightIcon}>
+                  <FontAwesome
+                    name="users"
+                    size={18}
+                    color={Colors.primaryColor}
+                  />
+                </View>
+                <View>
+                  <Text style={styles.highlightTxt}>Số lượng mẫu đất</Text>
+                  <Text style={styles.highlightTxtVal}>{listing.duration}</Text>
+                </View>
+              </View>
+              <View style={{ flexDirection: "row" }}>
+                <View style={styles.highlightIcon}>
+                  <Ionicons name="star" size={18} color={Colors.primaryColor} />
+                </View>
+                <View>
+                  <Text style={styles.highlightTxt}>Đánh giá</Text>
+                  <Text style={styles.highlightTxtVal}>{listing.rating}</Text>
+                </View>
+              </View>
+            </View>
+
+            <Text style={styles.listingDetails}>{listing.description}</Text>
+          </View>
+        </Animated.ScrollView>
+      </View>
+
+      <Animated.View style={styles.footer} entering={SlideInDown.delay(200)}>
+        <TouchableOpacity
+          onPress={() => setModalVisible(true)}
+          style={[styles.footerBtn, styles.footerBookBtn]}
+        >
+          <Text style={styles.footerBtnTxt}>Đặt Ngay</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {}} style={styles.footerBtn}>
+          <Text style={styles.footerBtnTxt}>${listing.price}</Text>
+        </TouchableOpacity>
+      </Animated.View>
+
+      <Modal
+        visible={modalVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Chọn mẫu đất</Text>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <View style={styles.slotsContainer}>{renderSlots()}</View>
+            </ScrollView>
+            <TouchableOpacity
+              style={styles.modalButton}
+              onPress={handleBookNow}
+            >
+              <Text style={styles.modalButtonText}>Đặt đất</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.modalButton, styles.modalCloseButton]}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.modalButtonText}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </>
+  );
+};
+
+export default ListingDetails;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.white,
+  },
+  image: {
+    width: width,
+    height: IMG_HEIGHT,
+  },
+  contentWrapper: {
+    padding: 20,
+    backgroundColor: Colors.white,
+  },
+  listingName: {
+    fontSize: 24,
+    fontWeight: "500",
+    color: Colors.black,
+    letterSpacing: 0.5,
+  },
+  listingLocationWrapper: {
+    flexDirection: "row",
+    marginTop: 5,
+    marginBottom: 10,
+    alignItems: "center",
+  },
+  listingLocationTxt: {
+    fontSize: 14,
+    marginLeft: 5,
+    color: Colors.black,
+  },
+  highlightWrapper: {
+    flexDirection: "row",
+    marginVertical: 20,
+    justifyContent: "space-between",
+  },
+  highlightIcon: {
+    backgroundColor: "#F4F4F4",
+    paddingHorizontal: 8,
+    paddingVertical: 5,
+    borderRadius: 8,
+    marginRight: 5,
+    alignItems: "center",
+  },
+  highlightTxt: {
+    fontSize: 12,
+    color: "#999",
+  },
+  highlightTxtVal: {
+    fontSize: 14,
+    fontWeight: "600",
+  },
+  listingDetails: {
+    fontSize: 16,
+    color: Colors.black,
+    lineHeight: 25,
+    letterSpacing: 0.5,
+  },
+  footer: {
+    flexDirection: "row",
+    position: "absolute",
+    bottom: 0,
+    padding: 20,
+    paddingBottom: 30,
+    width: width,
+  },
+  footerBtn: {
+    flex: 1,
+    backgroundColor: Colors.black,
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  footerBookBtn: {
+    flex: 2,
+    backgroundColor: Colors.primaryColor,
+    marginRight: 20,
+  },
+  footerBtnTxt: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: "600",
+    textTransform: "uppercase",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  modalContent: {
+    width: "80%",
+    backgroundColor: Colors.white,
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  slotsContainer: {
+    flexDirection: "row",
+  },
+  slotRow: {
+    flexDirection: "column",
+  },
+  slotItem: {
+    width: 70,
+    height: 70,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: Colors.black,
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 5,
+    marginVertical : 4
+  },
+  selectedSlotItem: {
+    backgroundColor: Colors.primaryColor,
+    
+  },
+  slotText: {
+    color: Colors.black,
+  },
+  selectedSlotText: {
+    color: Colors.white,
+  },
+  modalButton: {
+    backgroundColor: Colors.primaryColor,
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 20,
+    alignItems: "center",
+    width: "100%",
+  },
+  modalCloseButton: {
+    backgroundColor: Colors.black,
+    marginTop: 10,
+  },
+  modalButtonText: {
+    color: Colors.white,
+    fontWeight: "bold",
+  },
+});
